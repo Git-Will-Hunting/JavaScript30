7310b2b5a5823d771eb9d9598dbc0b7dee3eb131
git-will-hunting <58862067+Git-Will-Hunting@users.noreply.github.com>
2023-04-03 01:07:33 -0400

    Webcam 19 Stopped on filters due to memory

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -3,3 +3,78 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+function getVideo() {
+    const constraints ={
+        video: {
+            width: {exact: 640},
+            height: {exact: 480}
+        },
+        audio: false
+    };
+    navigator.mediaDevices.getUserMedia(constraints)
+    .then(localMediaStream => {
+        console.log(localMediaStream);
+        // video.src = window.URL.createObjectURL(localMediaStream);
+        video.srcObject = localMediaStream;
+        video.play();
+
+    })
+    .catch(err => {
+        console.error('Webcam not available');
+        alert('Webcam not available');
+    });
+}
+
+function paintToCanvas() {
+    const width = video.videoWidth;
+    const height = video.videoHeight;
+    canvas.width = width;
+    canvas.height = height;
+
+    return setInterval(() => {
+        ctx.drawImage(video, 0, 0, width, height);
+        // take pixels out
+        let pixels = ctx.getImageData(0, 0, width, height);
+        // mess with them
+        // pixels = redEffect(pixels);
+        pixels = rgbSplit(pixels);
+        // put them back
+        ctx.putImageData(pixels, 0, 0);
+    }, 20);
+}
+
+function takePhoto() {
+    //play sound
+    snap.currentTime = 0;
+    snap.play();
+    // take data out of canvas
+    const data = canvas.toDataURL('image/png');
+    const link = document.createElement('a');
+    link.href = data;
+    link.setAttribute('download', 'capture');
+    link.innerHTML = `<img src="${data}" alt="capture"/>`;
+    strip.insertBefore(link, strip.firstChild);  
+}
+
+function redEffect(pixels){
+    for(let i = 0; i <pixels.data.length; i+=4) {
+        pixels.data[i +0] = pixels.data[i + 0] + 100; // Red
+        pixels.data[i +1] = pixels.data[i + 1] - 50; // Green
+        pixels.data[i +2] = pixels.data[i + 2] * 0.5; // Blue
+    }
+    return pixels
+}
+
+function rgbSplit(pixels){
+    for(let i = 0; i <pixels.data.length; i+=4) {
+        pixels.data[i - 400] = pixels.data[i + 0]; // Red
+        pixels.data[i + 400] = pixels.data[i + 1]; // Green
+        pixels.data[i + 500] = pixels.data[i + 2]; // Blue
+    }
+    return pixels
+}
+
+getVideo();
+
+video.addEventListener('canplay', paintToCanvas);
