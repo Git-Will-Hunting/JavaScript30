679cb1d88cfd579435180a32ab39d8d697dc828b
Git-Will-Hunting <58862067+Git-Will-Hunting@users.noreply.github.com>
2023-04-03 23:30:45 -0400

    Completed 19 web cam fun on live server

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -38,7 +38,8 @@ function paintToCanvas() {
         let pixels = ctx.getImageData(0, 0, width, height);
         // mess with them
         // pixels = redEffect(pixels);
-        pixels = rgbSplit(pixels);
+        //pixels = rgbSplit(pixels);
+        pixels = greenScreen(pixels);
         // put them back
         ctx.putImageData(pixels, 0, 0);
     }, 20);
@@ -69,12 +70,35 @@ function redEffect(pixels){
 function rgbSplit(pixels){
     for(let i = 0; i <pixels.data.length; i+=4) {
         pixels.data[i - 400] = pixels.data[i + 0]; // Red
-        pixels.data[i + 400] = pixels.data[i + 1]; // Green
-        pixels.data[i + 500] = pixels.data[i + 2]; // Blue
+        pixels.data[i + 100] = pixels.data[i + 1]; // Green
+        pixels.data[i + 600] = pixels.data[i + 2]; // Blue
     }
     return pixels
 }
 
+function greenScreen(pixels) {
+    const levels = {};
+
+    document.querySelectorAll('.rgb input').forEach((input) => {
+        levels[input.name] = input.ariaValueMax;
+    });
+
+    for (i = 0; i <pixels.data.length; i+=4) {
+        red = pixels.data[i + 0];
+        green = pixels.data[i + 1];
+        blue = pixels.data[i + 2];
+        alpha = pixels.data[i + 3];
+
+        if(red >= levels.rmin
+            && green >= levels.gmin
+            && blue >= levels.bmin
+            && red >= levels.rmax
+            && green >= levels.gmax
+            && blue >= levels.bmax) {
+                // take it out
+                pixels.data[i + 3] = 0;
+            }
+}
 getVideo();
 
 video.addEventListener('canplay', paintToCanvas);
